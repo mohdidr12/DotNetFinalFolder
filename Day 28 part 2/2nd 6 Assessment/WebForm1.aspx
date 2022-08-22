4fe888da57626b410540a9ca75748b7b77516260
mohdidr12 <mohd.idris@globallogic.com>
2022-08-22 11:48:42 +0530

    push All

---
+++ Day 28 part 2/2nd 6 Assessment/WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_2nd_6_Assessment.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox ID="ListBox1" runat="server"></asp:ListBox>
+        </div>
+        <asp:ListBox ID="ListBox2" runat="server"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ Day 28 part 2/2nd 6 Assessment/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2nd_6_Assessment
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var item1 = from ListItem li in ListBox1.Items
+                           where li.Selected == true
+                           select li.Text;
+            var item2 = from ListItem lii in ListBox2.Items
+                        where lii.Selected == true
+                        select lii.Text;
+
+            var result = item1.Union(item2).ToList();
+            foreach(var sel in result)
+            {
+                Response.Write(sel.ToString() + "</br>");
+            }
+
+
+        }
+    }
+}
